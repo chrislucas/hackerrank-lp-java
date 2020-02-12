9997c44d036932b70f487fbc1f8ddc1feb7660af
chrislucas <christoffer.luccas@gmail.com>
2020-02-12 08:43:15 -0300

    regex capturing

--- strings/JavaRegex/src/solution/MatchingNumericRange.kt
+++ strings/JavaRegex/src/solution/MatchingNumericRange.kt
@@ -14,6 +14,9 @@ package solution
  * O motor de RE trata o '0' como um caracter unico e '255' como 3 caracteres
  * para fazer uma regex que capture um intervalo de numeros entre 0-255 por exemplo
  * precisamos de uma regex que capture de 1 a 3 caracteres
+ *
+ * Estudo de regex para capturar pattern Ipv4 e Ipv6
+ * https://riptutorial.com/regex/example/14146/match-an-ip-address
  * */
 
 fun testRegex2CharSequential() {
@@ -23,19 +26,33 @@ fun testRegex2CharSequential() {
 
 fun testRegexIntervalNumbersFrom0to255() {
     // 0-9, 10-99, 100-199, 200-249, 250-255
-    val regex = Regex("([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])")
+    val regex = Regex("^[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]$")
     (0..256).forEach { value ->
         println("$value -> ${value.toString().matches(regex)}")
     }
 }
 
-/**
- *
- * */
-fun testRegexRangeNumber(start: Int, end: Int) {
-    val regex = Regex("")
+fun testRegexMatchIpV4Address() {
+    val regex = Regex("^(([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\\.){3}([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])$")
+
+    arrayOf("333.123.123.123"
+        , "....", "123.123.123.123", "0.0.0.0", "200.200.200.200", "000.000.000.000"
+        ,"000.12.234.23.23","666.666.23.23",".213.123.23.32","23.45.22.32.", "200.200.200.256"
+        ,"0.19.19.19"
+    ).forEach {
+        println("$it -> ${it.matches(regex)}")
+    }
+}
+
+fun testCharSet() {
+    arrayOf(
+        "012", "000",
+        "111", "222", "0", "1", "2", "00", "01"
+    ).forEach {
+        println(it.matches(Regex("[0-2]{1,3}")))
+    }
 }
 
 fun main() {
-    testRegexIntervalNumbersFrom0to255()
+    testRegexMatchIpV4Address()
 }
